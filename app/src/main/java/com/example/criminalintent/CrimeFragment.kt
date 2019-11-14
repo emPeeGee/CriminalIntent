5c22541d5ea765faf324070a630128cb16fe30fc
emPeeGee <eempeegee@gmail.com>
2019-11-14 21:05:38 +0200

    Added fragment arguments and Room update statements

--- app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -8,14 +8,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.lifecycle.Observer
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_crime.*
+import java.util.*
 
+private const val ARG_CRIME_ID = "crimeId";
 
 class CrimeFragment : Fragment() {
 
     private lateinit var titleField: EditText
     private lateinit var dateButton: Button
 
+    private val crimeDetailViewModel: CrimeDetailViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeDetailViewModel::class.java)
+    }
 
     private lateinit var crime: Crime
 
@@ -23,6 +31,9 @@ class CrimeFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         crime = Crime()
+        val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
+        crimeDetailViewModel.loadCrime(crimeId = crimeId)
+
     }
 
     override fun onCreateView(
@@ -44,6 +55,18 @@ class CrimeFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeDetailViewModel.crimeLiveData.observe(
+            viewLifecycleOwner, Observer { crime ->
+                crime?.let {
+                    this.crime = crime
+                    updateUI()
+                }
+            }
+        )
+    }
+
     override fun onStart() {
         super.onStart()
 
@@ -62,5 +85,32 @@ class CrimeFragment : Fragment() {
         }
 
         titleField.addTextChangedListener(titleWatcher)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        crimeDetailViewModel.saveCrime(crime)
+    }
+
+    private fun updateUI() {
+        titleField.setText(crime.title)
+        dateButton.text = crime.date.toString()
+        crime_solved.apply {
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
+        }
+
+    }
+
+    companion object {
+        fun newInstance(crimeId: UUID): CrimeFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_CRIME_ID, crimeId);
+            }
+
+            return CrimeFragment().apply {
+                arguments = args
+            }
+        }
     }
 }
--- app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -6,6 +6,7 @@ import androidx.room.Room
 import com.example.criminalintent.database.CrimeDatabase
 import java.lang.IllegalStateException
 import java.util.*
+import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "crime-database"
 
@@ -15,10 +16,21 @@ class CrimeRepository private constructor(context: Context) {
         context.applicationContext, CrimeDatabase::class.java, DATABASE_NAME).build()
 
     private val dao = database.crimeDao()
+    private val executor = Executors.newSingleThreadExecutor()
 
     fun getCrimes(): LiveData<List<Crime>> = dao.getCrimes()
     fun getCrime(uuid: UUID): LiveData<Crime?> = dao.getCrime(uuid)
+    fun updateCrime(crime: Crime) {
+        executor.execute {
+            dao.updateCrime(crime)
+        }
+    }
 
+    fun addCrime(crime: Crime) {
+        executor.execute {
+            dao.addCrime(crime)
+        }
+    }
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
