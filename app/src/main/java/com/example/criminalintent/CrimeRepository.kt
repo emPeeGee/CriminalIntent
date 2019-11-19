15ebf47c5d4501e76cdbfb97c240c4d0570531fc
emPeeGee <eempeegee@gmail.com>
2019-11-19 21:01:28 +0200

    Added camera support

--- app/src/main/java/com/example/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.Room
 import com.example.criminalintent.database.CrimeDatabase
 import com.example.criminalintent.database.migration_1_2
+import java.io.File
 import java.lang.IllegalStateException
 import java.util.*
 import java.util.concurrent.Executors
@@ -20,6 +21,8 @@ class CrimeRepository private constructor(context: Context) {
 
     private val dao = database.crimeDao()
     private val executor = Executors.newSingleThreadExecutor()
+    private val filesDir = context.applicationContext.filesDir
+
 
     fun getCrimes(): LiveData<List<Crime>> = dao.getCrimes()
 
@@ -36,6 +39,9 @@ class CrimeRepository private constructor(context: Context) {
             dao.addCrime(crime)
         }
     }
+
+    fun getPhotoFile(crime: Crime): File = File(filesDir, crime.photoFileName)
+
 
     companion object {
         private var INSTANCE: CrimeRepository? = null
